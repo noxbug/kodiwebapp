bf1eab5ad45e50928625522bb30054c9570dc4d6
pieterjan <spruitje1>
2018-06-20 21:19:23 +0200

    lazy push

--- kodiwebapp/urls.py
+++ kodiwebapp/urls.py
@@ -2,8 +2,19 @@ from django.urls import path
 
 from . import views
 
+app_name = 'kodiwebapp'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    ### MENU ###
+    path('translate_sub', views.translate_sub, name='translate_sub'),
+    path('next_sub', views.next_sub, name='next_sub'),
+    path('update', views.update, name='update'),
+    ### PLAYER ###
     path('play/', views.play, name='play'),
     path('stop/', views.stop, name='stop'),
+    path('fast_forward/', views.fast_forward, name='fast_forward'),
+    path('fast_rewind/', views.fast_rewind, name='fast_rewind'),
+    ### CONTROL ###
+    path('control/', views.control, name='control'),
 ]
--- kodiwebapp/views.py
+++ kodiwebapp/views.py
@@ -1,22 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 import lib.kodirpc as kodi
 
-
+# Connect to kodi
 kodi.connection.connect('192.168.1.10')
 
 # Create your views here.
 
+
 def index(request):
-    #context = {'kodi': kodi}
-    #return render(request, 'kodiwebapp/base/base.html')
-    return HttpResponse('You are at the main site')
+    kodi.nowplaying.update()
+    context = {'kodi': kodi}
+    return render(request, 'kodiwebapp/now_playing.html', context)
 
 
+### MENU ###
+def translate_sub(request):
+    return HttpResponse('translate sub page')
+
+
+def next_sub(request):
+    return HttpResponse('next sub page')
+
+
+def update(request):
+    return HttpResponse('update library page')
+
+
+### PLAYER ###
 def play(request):
-    return HttpResponse('play/pause')
+    kodi.player.play_pause()
+    return redirect('kodiwebapp:index')
 
 
 def stop(request):
-    return HttpResponse('Stopping NOW!')
+    kodi.player.stop()
+    return redirect('kodiwebapp:index')
+
+
+def fast_forward(request):
+    kodi.player.fast_forward()
+    return redirect('kodiwebapp:index')
+
+
+def fast_rewind(request):
+    kodi.player.fast_rewind()
+    return redirect('kodiwebapp:index')
+
+
+### CONTROL ###
+def control(request):
+    return HttpResponse('control page!')
+
